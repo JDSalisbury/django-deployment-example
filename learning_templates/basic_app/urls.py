8dcfb849bdcc79b0af0695dab5b255e7989a5dc5
Jeffrey Salisbury <jsalisbury@everest.local>
2018-10-17 09:30:14 -0400

    changed to course files

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from basic_app import views
 
-#Template taggin
+# SET THE NAMESPACE!
 app_name = 'basic_app'
 
 urlpatterns=[
-    url(r'^relative/$', views.relative, name='relative'),
+    url(r'^relative/$',views.relative,name='relative'),
     url(r'^other/$',views.other,name='other'),
 ]
